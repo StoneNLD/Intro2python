79163554e9d4c981c1e7e6a0d51c0f8ff3a8811d
Stijn Nass <stijnnass@hotmail.com>
2021-03-10 11:38:16 +0100

    moving forward

---
+++ helloworld/useful_modules.py
@@ -0,0 +1,19 @@
+#!/usr/local/env python
+
+#create CLI argument parsing
+import argparse
+
+#working with JSON
+import json
+
+#math operations
+import math
+
+#interact with the OS
+import os
+
+#python debugger
+import pdb
+
+#interacting with system spcific parameters anf functions
+import sys
